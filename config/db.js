c1310bfe21e65a82b9dab4a721a9b56643ed803c
burb3rrydev <112503617+burb3rrydev@users.noreply.github.com>
2024-11-28 20:04:13 -0700

    Rebuild bcrypt

--- config/db.js
+++ config/db.js
@@ -2,10 +2,11 @@ const mysql = require('mysql');
 
 // Create database connection
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'root', 
-  database: 'shoe_store'
+  host: 'nnmeqdrilkem9ked.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+  user: 'de1znxqd8t73eu8c',
+  password: 'tmbtxijqi18nxnwf', 
+  database: 'v1pydn5nn39ee97o',
+  port: '3306'
 });
 
 // Connect to the database
